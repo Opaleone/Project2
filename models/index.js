b73be5eac83166c499ab742f61613d1723cd0864
CoolComicsJ <ziggaton@gmail.com>
2022-11-08 13:26:29 -0600

    "Initial modeling"

--- models/index.js
+++ models/index.js
@@ -1,2 +1,13 @@
+const Character = require('./character');
+const Race = require('./race');
+
+Character.blongsTo(Race, {
+    foreignKey: 'race_id'
+})
+
+Race.hasMany(Character, {
+    foreignKey: 'character_id'
+})
 
 
+module.exports = {Character, Race}
---
+++ models/race.js
@@ -0,0 +1,56 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Race extends Model {}
+
+Race.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        speed: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        strength_bonus: {
+            type: DataTypes.INTEGER,
+            defaultValue: 0
+        },
+        dexterity_bonus: {
+            type: DataTypes.INTEGER,
+            defaultValue: 0
+        },
+        constitution_bonus: {
+            type: DataTypes.INTEGER,
+            defaultValue: 0
+        },
+        intelligence_bonus: {
+            type: DataTypes.INTEGER,
+            defaultValue: 0
+        },
+        wisdom_bonus: {
+            type: DataTypes.INTEGER,
+            defaultValue: 0
+        },
+        charisma_bonus: {
+            type: DataTypes.INTEGER,
+            defaultValue: 0
+        },
+    },
+    {
+
+    }
+)
+
+module.exports = Race;
