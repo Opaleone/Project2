ba8c6b42967c6a353b71551bfd876241049ffb7a
Leone Varando <106971202+Opaleone@users.noreply.github.com>
2022-11-09 19:55:31 -0600

    Updated character sheet display function to display based on API calls

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,11 @@
 const raceSelector = document.querySelector('#race-selector')
 const formSubmit = document.querySelector('#submit-input')
 
+// const { baseGenerate, modifierGenerate } = require('../../utils/randomNumGen')
+
 const apiUrl = 'https://www.dnd5eapi.co/api/'
 
-async function createCharacter(event) {
+async function updateCharacterSheet(event) {
   event.preventDefault()
 
   let raceName = document.querySelector('#race').value
@@ -18,10 +20,13 @@ async function createCharacter(event) {
   console.log(raceData)
   console.log(classData)
 
-  // fetch('/api/characters', () => {
-  //   method: 'POST'
-  // })
-  //   .then()
+  displayCharacterInfo(raceData, classData)
+  // storeCharacter()
+}
+
+async function storeCharacter() {
+  let newCharacter = await fetch('/api/characters')
+  
 }
 
 async function getRace(val) {
@@ -37,5 +42,62 @@ async function getClass(val) {
 
   return response
 }
+// raceData, classData
+function displayCharacterInfo(raceData, classData) {
+  let statArr = document.querySelectorAll('.stat')
+  let statModArr = document.querySelectorAll('.statmod')
+  let skillArr = document.querySelectorAll('.skill-num')
+  let speedNum = document.querySelector("input[name='speed']");
+  let hitDie = document.querySelector("input[name='remaininghd'")
 
-formSubmit.addEventListener('click', createCharacter)
+  // console.log(statArr)
+  console.log(statModArr)
+  // console.log(skillArr)
+
+  let basicStats = [15, 14, 13, 12, 10, 8]
+
+  for (let i = 0; i < statArr.length; i++) {
+    let randomNum = Math.floor(Math.random() * basicStats.length)
+
+    statArr[i].value = basicStats[randomNum];
+
+    // switch (basicStats[randomNum] > 10) {
+    //   case 12:
+    //     statModArr[i].value = "+1"
+    //     break;
+    //   case 14:
+    //     statModArr[i].value = "+2"
+    //     break;
+    //   case 16:
+    //     statModArr[i].value = "+3"
+    //     break;
+    //   case 18:
+    //     statModArr[i].value = "+4"
+    //     break;
+    //   case 20:
+    //     statModArr[i].value = "+5"
+    //     break;
+    //   default:
+    //     statModArr[i].value = 0
+    //     break;
+    // }
+
+    basicStats.splice(randomNum, 1)
+  }
+
+  for (let i = 0; i < skillArr.length; i++) {
+    skillArr[i].value = modifierGenerate()
+  }
+
+  speedNum.value = raceData.speed
+  hitDie.value = classData.hit_die
+}
+
+function modifierGenerate() {
+  let randomNum = Math.floor(Math.random() * 5)
+  randomNum *=  Math.round(Math.random()) ? 1 : -1;
+
+  return randomNum
+}
+
+formSubmit.addEventListener('click', updateCharacterSheet)
